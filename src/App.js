8bdecef16c7930e8debcffc77059e2fb66f8c946
Khairul Ashry Sabry <ashry@KISWIRE.MY>
2020-03-12 16:28:20 +0800

    edit card

--- src/App.js
+++ src/App.js
@@ -1,37 +1,66 @@
 import React from 'react';
 import './App.css';
+import Px from './asset/image/wire.jpg';
+import Px1 from './asset/image/bridge.jpg';
+import Px2 from './asset/image/staff.jpg';
 
 function App() {
   return (
     <div>
       <div className='bg'>
         <div className='header'>
-          <h1>Sales System</h1>
-          <button className='button' id='btn'>Get Started</button>
+          <h1>aasdas</h1>
+          <button className='button'>Get Started</button>
         </div>
-        <div class="container">
+        <div class="box">
           <div class="card">
             <div class="card__image-container">
-              <img class="card__image" src='./asset/image/aerial-view-of-bridge-1211856.jpg' alt=""></img>
+              <img class="card__image" src={Px} alt=""></img>
             </div>
       
             <svg class="card__svg" viewBox="0 0 800 500">
-
+              <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"/>
+              <path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"/>
+            </svg>
+            <div class="card__content">
+              <h1 class="card__title">Competitive Strength</h1>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <div class="box">
+          <div class="card">
+            <div class="card__image-container">
+              <img class="card__image" src={Px1} alt=""></img>
+            </div>
+      
+            <svg class="card__svg" viewBox="0 0 800 500">
               <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"/>
               <path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"/>
             </svg>
     
             <div class="card__content">
-              <h1 class="card__title">Lorem ipsum</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor praesentium at quod autem omnis, amet eaque unde perspiciatis adipisci possimus quam facere illo et quisquam quia earum nesciunt porro.</p>
+              <h1 class="card__title">Quality Innovation</h1>
+              <p></p>
             </div>
           </div>
         </div>
-        <div className='box1'>
-          <h2 className='text'>Bead Wire</h2>
-        </div>
-        <div className='box2'>
-          <h2 className='text'>Wire Rope</h2>
+        <div class="box">
+          <div class="card">
+            <div class="card__image-container">
+              <img class="card__image" src={Px2} alt=""></img>
+            </div>
+      
+            <svg class="card__svg" viewBox="0 0 800 500">
+              <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"/>
+              <path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"/>
+            </svg>
+    
+            <div class="card__content">
+              <h1 class="card__title">Zero NC</h1>
+              <p></p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
