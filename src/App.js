0a5d95c9e90d533a128303864bd9c32ed378671b
Khairul Ashry Sabry <ashry@KISWIRE.MY>
2020-04-21 17:24:52 +0800

    Learn to style using Material-Ui/Styles

--- src/App.js
+++ src/App.js
@@ -1,70 +1,99 @@
 import React from 'react';
 import './App.css';
 import Px from './asset/image/wire.jpg';
-import Px1 from './asset/image/bridge.jpg';
-import Px2 from './asset/image/staff.jpg';
+// import Px1 from './asset/image/bridge.jpg';
+// import Px2 from './asset/image/staff.jpg';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-function App() {
+const useStyles = makeStyles({
+  background: {
+    backgroundImage: {Px},
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundAttachment: 'fixed',
+  },
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+  card: {
+    display: 'inline-flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+});
+
+export default function MediaCard() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <div className='bg'>
-        <div className='header'>
-          <h1>aasdas</h1>
-          <button className='button'>Get Started</button>
-        </div>
-        <div class="box">
-          <div class="card">
-            <div class="card__image-container">
-              <img class="card__image" src={Px} alt=""></img>
-            </div>
-      
-            <svg class="card__svg" viewBox="0 0 800 500">
-              <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"/>
-              <path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"/>
-            </svg>
-            <div class="card__content">
-              <h1 class="card__title">Competitive Strength</h1>
-              <p></p>
-            </div>
-          </div>
-        </div>
-        <div class="box">
-          <div class="card">
-            <div class="card__image-container">
-              <img class="card__image" src={Px1} alt=""></img>
-            </div>
-      
-            <svg class="card__svg" viewBox="0 0 800 500">
-              <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"/>
-              <path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"/>
-            </svg>
-    
-            <div class="card__content">
-              <h1 class="card__title">Quality Innovation</h1>
-              <p></p>
-            </div>
-          </div>
-        </div>
-        <div class="box">
-          <div class="card">
-            <div class="card__image-container">
-              <img class="card__image" src={Px2} alt=""></img>
-            </div>
-      
-            <svg class="card__svg" viewBox="0 0 800 500">
-              <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"/>
-              <path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"/>
-            </svg>
-    
-            <div class="card__content">
-              <h1 class="card__title">Zero NC</h1>
-              <p></p>
-            </div>
-          </div>
-        </div>
+    <div className={classes.background}>
+      <div className={classes.card}>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={require ("./asset/image/wire.jpg")}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary" className="button">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
       </div>
+      <div className="card">
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={require ("./asset/image/wire.jpg")}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+              across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
     </div>
+  </div> 
   );
 }
-
-export default App;
